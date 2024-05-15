e4a563cd5502feedb4f515d53e5e56056c90285d
elliot <elliot@localhost.localdomain>
2024-05-15 15:47:32 +0300

    add data to db

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -7,11 +7,16 @@ client = MongoClient("mongodb+srv://morsewallace254:whoami@cluster0.5pxkesk.mong
 db = client["dreamteam"]
 collection = db["dreamteam_collection"]
 
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
 @app.route('/add', methods=['POST'])
 def add_item():
-    data = request.get_json()
-    collection.insert_one(data)
-    return 'Item added successfully!', 200
+    if request.method == 'POST':
+        data = request.form.get('data')
+        collection.insert_one({'data': data})
+        return 'Item added successfully!', 200
 
 if __name__ == '__main__':
     app.run()
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,2 +1,2 @@
 bind = "0.0.0.0:5000"  # Bind to all network interfaces on port 5000
-workers = 4            # Number of worker processes
+workers = 2            # Number of worker processes
